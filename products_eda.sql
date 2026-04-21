c1e985923f96443c3d5383b1430310abb688bd72
Gourav Tiwary <90273558+printgourav@users.noreply.github.com>
2026-04-21 08:23:54 +0530

    Update products_eda.sql

--- products_eda.sql
+++ products_eda.sql
@@ -109,6 +109,120 @@ SELECT
 FROM `playground027.my_eda_project.products`
 
 -- ============================================
+-- Purpose: Understand Missing values for all columns wise on percentage basis
+-- Observation: - category has 2.37 % missing value, indicating a low data incompleteness
+--              - subcategory has 2.37 % missing value, indicating a low data incompleteness
+--              - product_name is fully populated, 0 % missing value indcating data completeness.
+--              - product_line is fully populated, 0 % missing value indcating data completeness.
+--              - maintenance has 2.37 % missing value, indicating a low data incompleteness
+--              - cost is fully populated, 0 % missing value indcating data completeness.
+--              - start_date is fully populated, 0 % missing value indcating data completeness.
+-- 					
+-- =====================================
+SELECT 
+  (COUNT(*) - COUNT(category)) / COUNT(*) * 100.0 AS category_null_pct
+FROM `playground027.my_eda_project.products` ;
 
+SELECT 
+	  (COUNT(*) - COUNT(subcategory)) / COUNT(*) * 100.0 AS subcategory_null_pct
+FROM `playground027.my_eda_project.products` ;
+ 
+SELECT 
+		(COUNT(*) - COUNT(product_name)) / COUNT(*) * 100.0 AS product_name_null_pct
+FROM `playground027.my_eda_project.products` ;
+
+ SELECT 
+		(COUNT(*) - COUNT(maintenance)) / COUNT(*) * 100.0 AS maintenance_null_pct
+FROM `playground027.my_eda_project.products` ;
+
+SELECT
+   100.0 * COUNTIF(cost IS NULL) / COUNT(*) AS cost_null_pct
+FROM `playground027.my_eda_project.products`;
+
+SELECT
+   100.0 * COUNTIF(start_date IS NULL) / COUNT(*) AS date_null_pct
+FROM `playground027.my_eda_project.products` ;
+
+SELECT
+   100.0 * COUNTIF(product_line IS NULL) / COUNT(*) AS product_line_null_pct
+FROM `playground027.my_eda_project.products` ;
+
+-- ==========================================
+--  Purpose : To know quantity of such product that cost 0 
+--  Observation: -- Found such 2 products.
+				 -- Products are HL RoadFrame -Black-58 & HL RoadFrame -Red-58.
+                 -- Both products come from one category Components.
+				 -- Also they share one similar subcategory RoadFrames.
+-- ===========================================
+
+SELECT 
+   category,
+   subcategory, 
+   product_name
+FROM `playground027.my_eda_project.products`
+WHERE cost = 0
+GROUP BY category, subcategory, product_name ;
+
+-- ===========================================
+-- Purpose : To understand average cost by category
+-- Observation: --  Out of 4 categories `Bikes` has 
+--              	highest average cost 969.44 .
+--              --  Lowest is Accessories with 13.17 .
+--              --  Second highest out of 4 is components 
+--                  having a average cost 264.72 .
+--              --  Second lowest out of 4 is components 
+--                  having a average cost of 24.8 .
+--              --  For missing or unknown category 
+--                  has average cost of 28.57 .
+-- ===========================================
+SELECT 
+   category,
+   ROUND(AVG(cost),2) average_cost_category_wise
+FROM `playground027.my_eda_project.products`
+GROUP BY category;
+
+-- =============================================
+-- Purpose: 	    To understand quantity of products that exist
+--          	    within each subcategory.
+-- Observation:  --  Road Bikes (43) and Road Frames (33) are the two
+--    				 largest subcategories .
+--               --  20 out of 37 subcategories have only 1-3 products
+--    				 (Locks, Fenders, Chains etc.) — accessories are
+--   				 present .
+--               --  Jerseys, Shorts, Gloves, Helmets combined have very
+--    				 few products .
+--               --  NULL subcategory contains 7 products
+SELECT 
+  subcategory,
+  COUNT(product_id)
+FROM `playground027.my_eda_project.products`
+GROUP BY subcategory
+ORDER BY 2 DESC, 1 ASC;
+-- ===============================================
+-- Purpose: To Know expensive product cost within each subcategory
+-- Observation:  -- Road Bikes is expensive subcategory with products worth of 2171
+--               -- Mountain Bikes is second expensive subcategory with products worth of 1912
+SELECT 
+  subcategory,
+  COUNT(product_id),
+  max(cost) 
+FROM `playground027.my_eda_project.products`
+GROUP BY subcategory
+ORDER BY 2 DESC, 1 ASC ;
+
+-- ================================================
+-- Purpose: To know which subcategory has expensive products in quantity terms
+-- Observation:  -- Road frames, touring bikes exhibit multiple products at the maximum price point .
+--               -- In contrast, many subcategories such as caps, chainer, cleaner and etc have a single product defining the maximum cost .
+SELECT subcategory, COUNT(product_id) AS max_cost_count
+FROM `playground027.my_eda_project.products` p
+WHERE cost = (
+  SELECT MAX(cost)
+  FROM `playground027.my_eda_project.products`
+  WHERE subcategory = p.subcategory
+)
+GROUP BY subcategory
+ORDER BY max_cost_count DESC, subcategory ASC;
+-- ===================================================
 
  
